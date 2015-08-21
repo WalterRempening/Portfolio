8454acbeab1c53150476affd07ce955649026fc1
Walter Rempening Diaz <w.rempeningd@gmail.com>
2015-08-21 18:06:02 +0200

    Oh yes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var favicon = require( 'serve-favicon' );
 var logger = require( 'morgan' );
 var cookieParser = require( 'cookie-parser' );
 var bodyParser = require( 'body-parser' );
-var csrf = require( 'csurf' );
+var csurf = require( 'csurf' );
 var app = express();
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
