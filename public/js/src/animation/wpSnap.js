644e37cbaa180f0aaa4a7cc4d269b78b69c397de
Walter Rempening Diaz <w.rempeningd@gmail.com>
2015-09-04 02:15:23 +0200

    Animations looking good, modified svg home log quite a lot

--- public/js/src/animation/wpSnap.js
+++ public/js/src/animation/wpSnap.js
@@ -15,47 +15,144 @@
       replace: true,
       controller: function ( $scope ) {
         Snap.load( $scope.src, function ( f ) {
+
             $scope.el.append( f );
 
-            //var oval = Snap.select( 'g#Layer_3' );
-            //var  stripe = Snap.select( 'g#Layer_2 > polygon' );
-            //var titel = Snap.select( 'g#Layer_1 text > textpath' );
-            //var lenB = stripe.getTotalLength();
-            //var lenT = Snap.path.getTotalLength(titel);
-            //var lenT = 500;
+            //-----------------------------------------------
+            //  Animation Length Time
+            //-----------------------------------------------
+            var animationTime = {
+              stripes: 300,
+              textPath: 1500,
+              backgroundDrop: 900
+            };
 
-            //console.log( lenT );
+            //-----------------------------------------------
+            //  Name Titel
+            //-----------------------------------------------
+            var titelB = Snap.select( 'g.TitelB' ),
+                titelW = Snap.selectAll( 'g.TitelW > path' );
+            var delay = 0;
 
-            //titel.attr( {
-            //  stroke: '#FF0000',
-            //  strokeWidth: "2",
-            //  strokeMiterLimit: "200",
-            //  strokeDasharray: '10 10',
-            //  strokeDashOffset: lenT
-            //} ).animate( {
-            //    "stroke-dashoffset": 10
-            //  }, 4000, mina.easeinout
-            //);
+            titelW.forEach( function ( path ) {
+              var lenT = path.getTotalLength();
+              path
+                .attr( {
+                  stroke: '#FFF',
+                  fill: '#117bb2',
+                  strokeWidth: "2",
+                  "stroke-dasharray": lenT + " " + lenT,
+                  "stroke-dashoffset": lenT
+                } );
 
-            //stripe.attr( {
-            //  stroke: '#000000',
-            //  strokeWidth: "4",
-            //  strokeMiterLimit: "10",
-            //  strokeDasharray: "9 9",
-            //  strokeDashOffset: "1000.43",
-            //  "stroke-dasharray": lenB + " " + lenB,
-            //  "stroke-dashoffset": lenB
-            //} );
+              setTimeout( function () {
+                path.animate( {
+                  fill: '#fff',
+                  "stroke-dashoffset": 10
+                  //opacity:1
+                }, animationTime.textPath, mina.linear );
+              }, 300 + delay );
+              delay += 50;
+              console.log( delay );
+            } );
 
-            //setTimeout( function () {
-            //  oval.animate( { transform: 't300 150', opacity: 0 }, 1000,
-            //    mina.bounce );
-            //  stripe.animate( { "stroke-dashoffset": 10 }, 2500, mina.easeinout )
-            //}, 0 );
+
+            titelB.attr( {
+              opacity: 0,
+              transform: 't0 50'
+            } );
+
+            var animateTitelBackground = function () {
+              titelB.animate( {
+                opacity: 1,
+                transform: 't0 0'
+              }, animationTime.backgroundDrop, mina.easeIn );
+            };
+
+
+            //-----------------------------------------------
+            //  Background
+            //-----------------------------------------------
+            //-----------------------------------------------
+            //  Mono
+            //-----------------------------------------------
+            //-----------------------------------------------
+            //  Bottom Stripe
+            //-----------------------------------------------
+            var stripeTop    = Snap.select( 'polygon#BarT' ),
+                stripeGrad   = Snap.select( 'polygon#BarG' ),
+                stripeBottom = Snap.select( 'polygon#BarB' );
+
+            var stripePos = {
+              init: {
+                top: [ 82.9, 727.6, 82.9, 672.6, 82.9, 623.6, 142, 612.5, 142, 716.5 ],
+                mid: [ 624.8, 541.5, 624.8, 645.2, 659.9, 624.6, 659.9, 524.1 ],
+                bottom: [ 153.9, 740.4, 153.9, 870, 235.5, 855.3, 165.7, 799.2, 235.5, 725.7 ]
+              },
+              finish: {
+                top: [ 82.9, 727.6, 138.9, 668.6, 82.9, 623.6, 659.9, 520.6, 659.9, 624.6 ],
+                mid: [ 153.9, 740.4, 153.9, 870, 659.9, 624.6, 659.9, 524.1 ],
+                bottom: [ 153.9, 740.4, 153.9, 870, 748.7, 763.6, 678.9, 707.6, 748.7, 634 ]
+              }
+            };
+
+            stripeTop.attr( {
+              "opacity": 0,
+              "points": stripePos.init.top.toString()
+            } );
+
+            stripeGrad.attr( {
+              "opacity": 0,
+              "points": stripePos.init.mid.toString()
+            } );
+
+            stripeBottom.attr( {
+              "opacity": 0,
+              "points": stripePos.init.bottom.toString()
+            } );
+
+            //----------------------------------------------
+            var animateSt = function () {
+              stripeTop.animate( {
+                "opacity": 1,
+                "points": stripePos.finish.top.toString()
+              }, animationTime.stripes, mina.backout );
+            };
+
+            var animateSg = function () {
+              stripeGrad.animate( {
+                "opacity": 1,
+                "points": stripePos.finish.mid.toString()
+              }, animationTime.stripes, mina.easeinout );
+            };
+
+            var animateSb = function () {
+              stripeBottom.animate( {
+                "opacity": 1,
+                "points": stripePos.finish.bottom.toString()
+              }, animationTime.stripes, mina.backout );
+            };
+
+            //-----------------------------------------------
+            //  Stripe Text
+            //-----------------------------------------------
+            var craftB = Snap.select( '#CraftB' ),
+                codeB  = Snap.select( '#CodeB' ),
+                craftW = Snap.select( '#CraftW' ),
+                codeW  = Snap.select( '#CodeW' );
+
+
+            //-----------------------------------------------
+            //  Animiation Timeline
+            //-----------------------------------------------
+
+            setTimeout( animateTitelBackground, 900 );
+            setTimeout( animateSt, 300 );
+            setTimeout( animateSg, 500 );
+            setTimeout( animateSb, 800 );
 
           }
-        )
-        ;
+        );
       },
       scope: {
         src: '@'
